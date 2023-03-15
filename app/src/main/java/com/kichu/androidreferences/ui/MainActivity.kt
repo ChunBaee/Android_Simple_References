b82fe737171773ec55508c4904c9240b1c020221
ChunBaee <mljung10@naver.com>
2023-03-15 18:57:59 +0900

    fix: Architecture구조 수정

--- app/src/main/java/com/kichu/androidreferences/ui/MainActivity.kt
+++ app/src/main/java/com/kichu/androidreferences/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.kichu.androidreferences
+package com.kichu.androidreferences.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kichu.androidreferences.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
