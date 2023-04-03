e616f0992a298c35e9f6e3621ecb028672d17aa1
ChunBaee <mljung10@naver.com>
2023-04-03 17:32:12 +0900

    feat: Main에 ViewBinding 추가

--- app/src/main/java/com/kichu/androidreferences/ui/MainActivity.kt
+++ app/src/main/java/com/kichu/androidreferences/ui/MainActivity.kt
@@ -3,10 +3,13 @@ package com.kichu.androidreferences.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.kichu.androidreferences.R
+import com.kichu.androidreferences.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
